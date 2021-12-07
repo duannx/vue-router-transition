52d8b6cfce8a0404353535502114a6ecf296cb62
duannx <duannguyen@beeketing.net>
2021-12-07 17:10:34 +0700

    feat: move defaultClassTransition to props

--- src/RouterViewTransition.vue
+++ src/RouterViewTransition.vue
@@ -11,11 +11,18 @@ import { useRouter } from "vue-router";
 import { RouterMetaTransition } from "./types";
 
 export default defineComponent({
-  setup() {
+  props: {
+    defaultClassTransition: {
+      type: String,
+      required: false,
+      default: "transition-active"
+    }
+  },
+  setup(props) {
     const router = useRouter();
+    // Store router history. Not used yet.
     const history: Array<string> = [];
     // This class will be added to all the elements that are transitioning
-    const defaultClassTransition = "transition-active";
 
     router.afterEach((to, from) => {
       // Do not show animation on the first page load
@@ -31,6 +38,7 @@ export default defineComponent({
         .transitions as RouterMetaTransition;
       let transition = "";
 
+      // Choose the highest priority transition (lowest number)
       if (toTransistions && fromTransistions) {
         fromTransistions.priority < toTransistions.priority
           ? (transition = fromTransistions.out)
@@ -41,13 +49,17 @@ export default defineComponent({
         transition = fromTransistions.out;
       }
 
-      to.meta.transition = defaultClassTransition + " " + transition;
+      to.meta.transition = transition
+        ? props.defaultClassTransition + " " + transition
+        : "";
 
+      // Add the new path to the history.
       if (to.path !== from.path) {
         const beforeLast = history[history.length - 2];
         if (!beforeLast || beforeLast !== to.path) {
           history.push(to.path);
         } else {
+          // Remove the last path from the history in case the router go back to beforeLast router
           history.pop();
         }
       }
