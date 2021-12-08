5c9815cc3615c174d34888b7fcd7824882c6d096
duannx <duannguyen@beeketing.net>
2021-12-08 16:36:36 +0700

    build: update public path for github page

--- dev/src/router/index.ts
+++ dev/src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory, Router } from 'vue-router'
 import routes from './routes'
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory('/vue-router-transition/'),
     routes
 })
 export default router
--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -7,4 +7,5 @@ export default defineConfig({
   build: {
     outDir: '../docs'
   },
+  base: '/vue-router-transition/'
 })
