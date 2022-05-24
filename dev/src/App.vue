63a22a89e98712f57e25c217491ed53dfff98139
duannx <du02@goldenphoenix.vn>
2022-05-24 14:43:27 +0700

    demo: add menu on the top

--- dev/src/App.vue
+++ dev/src/App.vue
@@ -4,6 +4,34 @@
 </script>
 
 <template>
-  <RouterViewTransition defaultClassTransition="transition-active" />
+  <div class="menu">
+    <div class="menu__item">
+      <router-link to="/collection/1">Dog</router-link>
+    </div>
+    <div class="menu__item">
+      <router-link to="/collection/2">Cat</router-link>
+    </div>
+  </div>
+  <div class="dynamic-content">
+    <RouterViewTransition defaultClassTransition="transition-active" />
+  </div>
+  
 </template>
+
+<style lang="scss">
+.menu {
+  display: flex;
+  justify-content: center;
+  background: #eee;
+  position: relative;
+  z-index: 10;
+
+  &__item {
+    cursor: pointer;
+    padding: 10px 20px;
+    font-size: 2rem;
+    text-decoration: underline;
+  }
+}
+</style>
 
